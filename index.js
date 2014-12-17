813002b77dcf5190961250fd7f9a4703e166aa41
unknown <sih@WPC0848.intranet.ibo.de>
2014-12-17 15:19:09 +0100

    This commit closes the ldap connection after authenticate. Otherwise nodejs throws an error: This socket has been ended by the other party

--- index.js
+++ index.js
@@ -1,55 +1,57 @@
-var crypto = require('crypto')
-var assert = require('assert')
-var LdapAuth = require('ldapauth-fork')
-var parseDN = require('ldapjs').parseDN
+var crypto = require('crypto'),
+assert = require('assert'),
+LdapAuth = require('ldapauth-fork'),
+parseDN = require('ldapjs').parseDN;
 
-module.exports = Auth
+module.exports = Auth;
 
 function Auth(config, stuff) {
-  var self = Object.create(Auth.prototype)
-  self._users = {}
+	var self = Object.create(Auth.prototype);
+	self._users = {};
 
-  // config for this module
-  self._config = config
+	// config for this module
+	self._config = config;
 
-  // sinopia logger
-  self._logger = stuff.logger
+	// sinopia logger
+	self._logger = stuff.logger;
 
-  // TODO: Set more defaults
-  self._config.groupNameAttribute = self._config.groupNameAttribute || 'cn'
-  self._ldap = new LdapAuth(self._config.client_options)
+	// TODO: Set more defaults
+	self._config.groupNameAttribute = self._config.groupNameAttribute || 'cn';
 
-  return self
+	return self;
 }
 
 //
 // Attempt to authenticate user against LDAP backend
 //
-Auth.prototype.authenticate = function(user, password, callback) {
-  var self = this
+Auth.prototype.authenticate = function (user, password, callback) {
+	var self = this,
+	ldap = new LdapAuth(self._config.client_options);
+	ldap.authenticate(user, password, function (err, ldap_user) {
 
-  self._ldap.authenticate(user, password, function(err, ldap_user) {
-    if (err) {
-      // 'No such user' is reported via error
-      self._logger.warn({
-        user: user,
-        err: err,
-      }, 'LDAP error @{err}')
-      return callback(null, false)
-    }
+		if (err) {
+			// 'No such user' is reported via error
+			self._logger.warn({
+				user : user,
+				err : err,
+			}, 'LDAP error @{err}')
+			return callback(null, false);
+		}
 
-    if (ldap_user) {
-      var groups = [ user ]
-      if ('memberOf' in ldap_user) {
-        if (!Array.isArray(ldap_user.memberOf)) {
-          ldap_user.memberOf = [ ldap_user.memberOf ]
-        }
-        for (var i = 0; i < ldap_user.memberOf.length; i++) {
-          groups.push("%" + parseDN(ldap_user.memberOf[i]).rdns[0][self._config.groupNameAttribute])
-        }
-      }
-    }
+		if (ldap_user) {
+			var groups = [user];
+			if ('memberOf' in ldap_user) {
+				if (!Array.isArray(ldap_user.memberOf)) {
+					ldap_user.memberOf = [ldap_user.memberOf];
+				}
+				for (var i = 0; i < ldap_user.memberOf.length; i++) {
+					groups.push("%" + parseDN(ldap_user.memberOf[i]).rdns[0][self._config.groupNameAttribute]);
+				}
+			}
+		}
 
-    callback(null, groups)
-  })
+		ldap.close(function () {
+			callback(null, groups);
+		});
+	})
 }
